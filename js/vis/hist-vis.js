f454db7dd07072e667b579b1104798dc32f9c53f
mattdeveney <mdeveney@college.harvard.edu>
2016-12-07 12:34:08 -0500

    scatter

--- js/vis/hist-vis.js
+++ js/vis/hist-vis.js
@@ -34,14 +34,24 @@ HistVis.prototype.initVis = function () {
     vis.yScale = d3.scale.linear()
         .range([vis.height, 0]);
 
-    //axis
+    // x-axis
     vis.xAxis = d3.svg.axis()
         .scale(vis.xScale)
         .orient("bottom");
 
+    // y-axis
+    vis.yAxis = d3.svg.axis()
+        .scale(vis.yScale)
+        .orient("left");
+
+
     vis.svg.append("g")
         .attr("class", "x-axis axis")
         .attr("transform", "translate(0," + vis.height + ")");
+
+    vis.svg.append("g")
+        .attr("class", "y-axis axis")
+        .attr("transform", "translate(0,0)");
 
 
 //d3 tip
@@ -70,48 +80,64 @@ HistVis.prototype.wrangleData = function() {
 
     var vis = this;
 
-    vis.category = d3.select("#hist-category").property("value");
+    vis.xCategory = d3.select("#scat-xCategory").property("value");
+    vis.yCategory = d3.select("#scat-yCategory").property("value");
+
+    console.log(vis.xCategory);
+    console.log(vis.yCategory);
 
     // get data specific to category
     vis.catData = vis.parentData.map(function(d){
 
-        var raw = +d[vis.category];
+        var xRaw = +d[vis.xCategory];
+        var yRaw = +d[vis.yCategory];
 
-        var converted;
+        var xConverted;
+        var yConverted;
 
         // convert to appropriate units
-        if (vis.category == "PlanetaryMassJpt") {
+        if (vis.xCategory == "PlanetaryMassJpt") {
             // earth masses
-            converted = raw * 317.8;
-        } else if (vis.category == "RadiusJpt") {
+            xConverted = xRaw * 317.8;
+        } else if (vis.xCategory == "RadiusJpt") {
             // earth radii
-            converted = raw * 11.209;
-        } else if (vis.category == "PeriodDays") {
-            // period in years
-            converted = raw / 365.0;
+            xConverted = xRaw * 11.209;
         } else {
-            converted = raw;
+            xConverted = xRaw;
         }
+
+        // convert to appropriate units
+        if (vis.xCategory == "PlanetaryMassJpt") {
+            // earth masses
+            yConverted = yRaw * 317.8;
+        } else if (vis.xCategory == "RadiusJpt") {
+            // earth radii
+            yConverted = yRaw * 11.209;
+        } else {
+            yConverted = yRaw;
+        }
+
+        var converted = {x : xConverted, y : yConverted};
 
         return converted;
     });
-
-    if (vis.category == "PlanetaryMassJpt") {
-        // earth masses
-        $("#hist-x-label").text("Planetary Mass (in Earth Masses)");
-    } else if (vis.category == "RadiusJpt") {
-        // earth radii
-        $("#hist-x-label").text("Planetary Radius (in Earth Radii)");
-    } else if (vis.category == "PeriodDays") {
-        // earth radii
-        $("#hist-x-label").text("Orbital Period (Years)");
-    } else {
-        // earth radii
-        $("#hist-x-label").text("Orbital Radius (AU)");
-    }
+    //
+    // if (vis.category == "PlanetaryMassJpt") {
+    //     // earth masses
+    //     $("#hist-x-label").text("Planetary Mass (in Earth Masses)");
+    // } else if (vis.category == "RadiusJpt") {
+    //     // earth radii
+    //     $("#hist-x-label").text("Planetary Radius (in Earth Radii)");
+    // } else if (vis.category == "PeriodDays") {
+    //     // earth radii
+    //     $("#hist-x-label").text("Orbital Period (Days)");
+    // } else {
+    //     // earth radii
+    //     $("#hist-x-label").text("Orbital Radius (AU)");
+    // }
 
     vis.nonNullData = vis.catData.filter(function(d){
-        return (d !== 0 && d < 10000);
+        return (d.x != 0 && d.y != 0);
     });
 
     vis.displayData = vis.nonNullData;
@@ -123,44 +149,104 @@ HistVis.prototype.updateVisualization = function () {
 
     var vis = this;
 
+    vis.xScale.domain([0, d3.max(vis.displayData, function(d){
+        return d.x;
+    })]);
 
+    vis.yScale.domain([0,d3.max(vis.displayData, function (d){
+        return d.y;
+    })]);
 
-    vis.xScale.domain([d3.min(vis.displayData),d3.max(vis.displayData)]);
-
-    vis.bins = d3.layout.histogram()
-        .bins(vis.xScale.ticks(30))
-        (vis.displayData);
-
-    vis.yScale.domain([0, d3.max(vis.bins, function(d) { return d.length; })]);
-
-    console.log(vis.bins);
-
-
-    vis.bar = vis.svg.selectAll(".bar")
-        .data(vis.bins, function(d){ return d.x; });
-
-    vis.bar
-        .enter().append("g")
-        .attr("class", "bar")
-        .attr("transform", function(d) { return "translate(" + vis.xScale(d.x) + "," + vis.yScale(d.y) + ")"; });
-
-    vis.bar
-        .attr("transform", function(d) { return "translate(" + vis.xScale(d.x) + "," + vis.yScale(d.y) + ")"; })
-        .transition()
-        .duration(1000)
-        .attr("height", function(d) { return vis.height - vis.yScale(d.y); });
-
-    vis.bar.exit().remove();
-
-    vis.bar.append("rect")
-        .attr("x", 1)
-        .attr("width", vis.xScale(vis.bins[0].dx - vis.xScale(0)) - 1)
-        .attr("height", function(d) { return vis.height - vis.yScale(d.length); })
-        .attr("fill", "rgb(200,200,200)");
+    vis.svg.selectAll(".dot")
+        .data(vis.displayData)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("fill","rgb(185,185,185)")
+        .attr("r", 3.5)
+        .attr("cx", function(d){
+            return vis.xScale(d.x)})
+        .attr("cy", function(d){return vis.yScale(d.y)});
 
     vis.svg.select(".x-axis")
         .attr("transform", "translate(0," + vis.height + ")")
         .call(vis.xAxis);
 
+    vis.svg.select(".x-axis")
+        .append("text")
+        .attr("x", vis.width / 2 - 10)
+        .attr("y", 50)
+        .text(convertCategory(vis.xCategory));
+
+    vis.svg.select(".y-axis")
+        .attr("transform", "translate(0,0)")
+        .call(vis.yAxis);
+
+    vis.svg.select(".y-axis")
+        .append("text")
+        .attr("y", -50)
+        .attr("x", -200)
+        .text(convertCategory(vis.yCategory))
+        .attr("transform", "rotate(-90)")
 
 };
+
+function convertCategory(category) {
+    if (category == "PlanetaryMassJpt") {
+        // earth masses
+        return "Mass (in Earth Masses)";
+    } else if (category == "RadiusJpt") {
+        // earth radii
+        return "Radius (in Earth Radii)";
+    } else if (category == "PeriodDays") {
+        return "Orbital Period (Days";
+    } else {
+        return "Orbital Radius (AU)";
+    }
+};
+
+
+// OLD HISTOGRAM CODE
+
+// vis.bins = d3.layout.histogram()
+//     .bins(vis.xScale.ticks(30))
+//     (vis.displayData);
+//
+// vis.yScale.domain([0, d3.max(vis.bins, function(d) { return d.length; })]);
+
+// .on("mouseover", function(d) {
+//     tooltip.transition()
+//         .duration(200)
+//         .style("opacity", .9);
+//     tooltip.html(d["Cereal Name"] + "<br/> (" + xValue(d)
+//         + ", " + yValue(d) + ")")
+//         .style("left", (d3.event.pageX + 5) + "px")
+//         .style("top", (d3.event.pageY - 28) + "px");
+// })
+// .on("mouseout", function(d) {
+//     tooltip.transition()
+//         .duration(500)
+//         .style("opacity", 0);
+// });
+
+
+// vis.bar = vis.svg.selectAll(".bar")
+//     .data(vis.bins, function(d){ return d.x; });
+//
+// vis.bar
+//     .enter().append("g")
+//     .attr("class", "bar")
+//     // .attr("transform", function(d) { return "translate(" + vis.xScale(d.x) + "," + vis.yScale(d.y) + ")"; });
+//
+// vis.bar
+//     .attr("transform", function(d) { return "translate(" + vis.xScale(d.x) + "," + vis.yScale(d.y) + ")"; })
+//     .transition()
+//     .duration(1000)
+//     .attr("height", function(d) { return vis.height - vis.yScale(d.y); });
+//
+// vis.bar.exit().remove();
+//
+// vis.bar.append("rect")
+//     .attr("x", 1)
+//     .attr("width", vis.xScale(vis.bins[0].dx - vis.xScale(0)) - 1)
+//     .attr("height", function(d) { return vis.height - vis.yScale(d.length); })
+//     .attr("fill", "rgb(200,200,200)");
